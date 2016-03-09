5b388c1ba1259e5190589667e3a8b9eb8f211a10
Marcus <marcuswald@hotmail.com>
2016-03-09 22:03:03 +0100

    Setup SFML

--- AntColonyOptimization/AntColonyOptimization/Main.cpp
+++ AntColonyOptimization/AntColonyOptimization/Main.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include "Graph.h"
+#include <SFML/Window.hpp>
 
 int main()
 {
-	Graph<char> g;
-	g.AddNode('A');
-	g.AddNode('B');
-	g.AddNode('C');
-	g.AddNode('D');
-	g.AddNode('E');
-	g.AddNode('F');
-	g.AddNode('G');
+	sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	g.AddEdge(0, 1);
-	g.AddEdge(0, 2);
-	g.AddEdge(0, 3);
-	g.AddEdge(0, 4);
-	g.AddEdge(0, 5);
-	g.AddEdge(0, 6);
-
-
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
 
 	return 0;
 }
